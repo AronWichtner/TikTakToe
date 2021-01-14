5457636adc159234f108a1cacb83e81dbba0ab34
AronWichtner <aronwichtner@gmail.com>
2021-01-14 12:39:13 +0100

    added diag win

--- TikTakToe.py
+++ TikTakToe.py
@@ -33,30 +33,57 @@ def create_playboard():
 
 
 def check_for_winner_status(lbl):
-    if verticalwin(lbl):
+    playboard = create_playboard()
+    if verticalwin(playboard):
+        # disable buttons
         lbl["text"] = "{} wins!".format(activeplayer.name)
         return None
-    elif horizontalwin(lbl):
+    elif horizontalwin(playboard):
+        # disable buttons
+        lbl["text"] = "{} wins!".format(activeplayer.name)
+        return None
+    elif diagonalwin(playboard):
+        # disable buttons
+        lbl["text"] = "{} wins!".format(activeplayer.name)
         return None
 
 
-def verticalwin(lbl):
-    playboard = create_playboard()
+
+def verticalwin(playboard):
     statusrow0 = all(sign == activeplayer.sign for sign in playboard[0])
     statusrowe1 = all(sign == activeplayer.sign for sign in playboard[1])
     statusrowe2 = all(sign == activeplayer.sign for sign in playboard[2])
     if statusrow0:
         return True
-        #disable all buttons from buttons
     elif statusrowe1:
         return True
-        # disable all buttons from buttons
     elif statusrowe2:
         return True
 
 
-def horizontalwin(lbl):
-    playboard = create_playboard()
+def horizontalwin(playboard):
+    column1 = [playboard[0][0], playboard[1][0], playboard[2][0]]
+    column2 = [playboard[0][1], playboard[1][1], playboard[2][1]]
+    column3 = [playboard[0][2], playboard[1][2], playboard[2][2]]
+    statuscolumn1 = all(sign == activeplayer.sign for sign in column1)
+    statuscolumn2 = all(sign == activeplayer.sign for sign in column2)
+    statuscolumn3 = all(sign == activeplayer.sign for sign in column3)
+    if statuscolumn1:
+        return True
+    elif statuscolumn2:
+        return True
+    elif statuscolumn3:
+        return True
+
+def diagonalwin(playboard):
+    diag_left_to_right = [playboard[0][0], playboard[1][1], playboard[2][2]]
+    diag_right_to_left = [playboard[2][2], playboard[1][1], playboard[0][0]]
+    statusdiag_left_to_right = all(sign == activeplayer.sign for sign in diag_left_to_right)
+    statusdiag_right_to_left = all(sign == activeplayer.sign for sign in diag_right_to_left)
+    if statusdiag_left_to_right:
+        return True
+    elif statusdiag_right_to_left:
+        return True
 
 
 def switch_active_player():
