d664ea764327a345b8af0fdedb35101f3048c155
AronWichtner <aronwichtner@gmail.com>
2021-02-04 21:51:43 +0100

    add rest_game_to_aivsplminimax

--- AIvsPl.py
+++ AIvsPl.py
@@ -15,3 +15,21 @@ def reset_game_to_aivspl(btns, pl1txt, drawtxt, pl2txt, new_game_btn, lblone, lb
         lbltwo["text"] = "-"
     else:
         return None  # Player has to make move
+
+
+def reset_game_to_aivsplminimax(btns, pl1txt, drawtxt, pl2txt, new_game_btn, lblone, lbltwo):
+    game.run_ai_vs_ai = False
+    game.set_game_for_aivspl()
+    reset_score(pl1txt, drawtxt, pl2txt)
+    new_game_btn["state"] = DISABLED
+    reset_playboard(btns, lblone, lbltwo)
+    enablebuttons(btns)
+    if game.activeplayer.name == game.ai.name:
+        playboard = create_playboard(btns)
+        bestmove = game.ai.find_best_move(playboard)
+        game.ai.makeBestMove(bestmove, btns)
+        switch_active_player()
+        lblone["text"] = "{}, put your sign!".format(game.activeplayer.name)
+        lbltwo["text"] = "-"
+    else:
+        return None  # Player has to make move
