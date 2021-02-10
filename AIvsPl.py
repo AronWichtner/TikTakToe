1f8edb6005b6d044c37ded5e474db1c4bc4a686e
AronWichtner <aronwichtner@gmail.com>
2021-02-10 18:34:20 +0100

    add MiniMax algorithm

--- AIvsPl.py
+++ AIvsPl.py
@@ -1,22 +1,6 @@
 from Gamefunctions import *
 
 
-def reset_game_to_aivspl(btns, pl1txt, drawtxt, pl2txt, new_game_btn, lblone, lbltwo):
-    game.run_ai_vs_ai = False
-    game.set_game_for_aivspl()
-    reset_score(pl1txt, drawtxt, pl2txt)
-    new_game_btn["state"] = DISABLED
-    reset_playboard(btns, lblone, lbltwo)
-    enablebuttons(btns)
-    if game.activeplayer.name == game.ai.name:
-        game.ai.setsignrandom(btns)
-        switch_active_player()
-        lblone["text"] = "{}, put your sign!".format(game.activeplayer.name)
-        lbltwo["text"] = "-"
-    else:
-        return None  # Player has to make move
-
-
 def reset_game_to_aivsplminimax(btns, pl1txt, drawtxt, pl2txt, new_game_btn, lblone, lbltwo):
     game.run_ai_vs_ai = False
     game.set_game_for_aivspl()
@@ -33,3 +17,5 @@ def reset_game_to_aivsplminimax(btns, pl1txt, drawtxt, pl2txt, new_game_btn, lbl
         lbltwo["text"] = "-"
     else:
         return None  # Player has to make move
+
+
