30cda0b8b34b94db6a9862c84d70b59d2e98c395
AronWichtner <aronwichtner@gmail.com>
2021-02-10 18:35:37 +0100

    add tempwinsign to Game class

--- GameSetup.py
+++ GameSetup.py
@@ -8,7 +8,9 @@ class Game:
     player2 = Player
     ai = AI
     activeplayer = ActivePlayer
+    status = False
     winner = Winner
+    tempwinsign = str
     win_count_pl1 = int
     win_count_pl2 = int
     draw_count = int
@@ -22,45 +24,48 @@ class Game:
         if gamemode == 1:
             self.gamemode = 1
             if pick == 1:
-                self.activeplayer = ActivePlayer(self.player1.sign, self.player1.name)
+                self.activeplayer = ActivePlayer(self.player1.sign, self.player1.name, self.player2.sign)
             else:
-                self.activeplayer = ActivePlayer(self.player2.sign, self.player2.name)
+                self.activeplayer = ActivePlayer(self.player2.sign, self.player2.name, self.player1.sign)
         elif gamemode == 2:
             self.gamemode = 2
             if pick == 1:
-                self.activeplayer = ActivePlayer(self.player1.sign, self.player1.name)
+                self.activeplayer = ActivePlayer(self.player1.sign, self.player1.name, self.ai.sign)
             else:
-                self.activeplayer = ActivePlayer(self.ai.sign, self.ai.name)
+                self.activeplayer = ActivePlayer(self.ai.sign, self.ai.name, self.player1.sign)
         elif gamemode == 3:
             self.gamemode = 3
             if pick == 1:
-                self.activeplayer = ActivePlayer(self.ai1.sign, self.ai1.name)
+                self.activeplayer = ActivePlayer(self.ai1.sign, self.ai1.name, self.ai2.sign)
             else:
-                self.activeplayer = ActivePlayer(self.ai2.sign, self.ai2.name)
+                self.activeplayer = ActivePlayer(self.ai2.sign, self.ai2.name, self.ai1.sign)
 
     def set_game_for_plvspl(self):
+        self.status = False
         self.player1 = Player("X", "PlayerX")
         self.player2 = Player("O", "PlayerO")
         self.choose_who_starts_first(1)
-        self.winner = Winner(False)
+        self.winner = Winner()
         self.win_count_pl1 = 0
         self.win_count_pl2 = 0
         self.draw_count = 0
 
     def set_game_for_aivspl(self):
+        self.status = False
         self.player1 = Player("X", "PlayerX")
         self.ai = AI("O", "AI")
         self.choose_who_starts_first(2)
-        self.winner = Winner(False)
+        self.winner = Winner()
         self.win_count_pl1 = 0
         self.win_count_pl2 = 0
         self.draw_count = 0
 
     def set_game_for_aivai(self):
+        self.status = False
         self.ai1 = AI("X", "PlayerX")
         self.ai2 = AI("O", "PlayerO")
         self.choose_who_starts_first(3)
-        self.winner = Winner(False)
+        self.winner = Winner()
         self.win_count_pl1 = 0
         self.win_count_pl2 = 0
         self.draw_count = 0
