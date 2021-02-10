3b8498e22a3151de5c3055ae5f3d4309958a14a1
AronWichtner <aronwichtner@gmail.com>
2021-02-10 18:33:26 +0100

    add MiniMax algorithm

--- AIvsAI.py
+++ AIvsAI.py
@@ -15,21 +15,27 @@ def ai_vs_ai_game(window, btns, pl1txt, drawtxt, pl2txt, new_game_btn, lblone, l
     for i in range(moves):
         if game.run_ai_vs_ai:
             if game.activeplayer.name == game.ai1.name:
-                game.ai1.setsignrandom(btns)
-                game_over = check_for_game_status(btns)
-                if game_over is True or game_over is None:
-                    return end_game(game_over, btns, pl1txt, drawtxt, pl2txt, lblone, lbltwo, new_game_btn)
-                if not game_over:
+                playboard = create_playboard(btns)
+                bestmove = game.ai1.find_best_move(playboard)
+                game.ai1.makeBestMove(bestmove, btns)
+                playboard = create_playboard(btns)
+                game_status = check_for_game_status(playboard)
+                if game.status is True or game.status is None:
+                    return end_game(game_status, btns, pl1txt, drawtxt, pl2txt, lblone, lbltwo, new_game_btn)
+                if not game.status:
                     switch_active_player()
                     lblone["text"] = "{}, put your sign!".format(game.activeplayer.name)
                     lbltwo["text"] = "-"
                 window.update()
             elif game.activeplayer.name == game.ai2.name:
-                game.ai2.setsignrandom(btns)
-                game_over = check_for_game_status(btns)
-                if game_over:
-                    return end_game(game_over, btns, pl1txt, drawtxt, pl2txt, lblone, lbltwo, new_game_btn)
-                if not game_over:
+                playboard = create_playboard(btns)
+                bestmove = game.ai2.find_best_move(playboard)
+                game.ai2.makeBestMove(bestmove, btns)
+                playboard = create_playboard(btns)
+                game_status = check_for_game_status(playboard)
+                if game.status is True or game.status is None:
+                    return end_game(game_status, btns, pl1txt, drawtxt, pl2txt, lblone, lbltwo, new_game_btn)
+                if not game.status:
                     switch_active_player()
                     lblone["text"] = "{}, put your sign!".format(game.activeplayer.name)
                     lbltwo["text"] = "-"
